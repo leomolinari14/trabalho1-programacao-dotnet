9182b0e946642aa963cea19a26796a66882f4352
Leonardo Molinari <207478123+leomolinari14@users.noreply.github.com>
2026-09-16 11:18:45 -0300

    Implementa exibicao aberta com reflection

--- Questao2Reflection/Program.cs
+++ Questao2Reflection/Program.cs
@@ -2,6 +2,16 @@
 
 class Program
 {
+    public static void ExibirDadosAberto(object objeto)
+    {
+        Type tipo = objeto.GetType();
+
+        foreach (var propriedade in tipo.GetProperties())
+        {
+            Console.WriteLine($"{propriedade.Name}: {propriedade.GetValue(objeto)}");
+        }
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Questão 2 - Reflection aberta e controlada");
